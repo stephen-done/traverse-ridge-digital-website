c470920595a54e7ab420036424226e73cacaeb87
Stephen Done <stephen.done@icloud.com>
2026-02-07 20:35:50 -0700

    add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://traverseridgedigital.com',
+  base: '/',
   output: 'static',
   integrations: [react()],
 
